fd328efa10d3714166189ab6c469f8487c274503
Carolyn Selheim <carolynselheim@Carolyns-MacBook-Pro.local>
2017-12-14 10:46:30 -0700

    Best Wine Friends progress

---
+++ backend/food.js
@@ -0,0 +1,101 @@
+var food = [
+  {
+    id: 1,
+    ingredient: "Vegetables",
+    flavors: [
+      {
+        flavorId: 1,
+        profile: "Earthy and Savory",
+        description: "root vegetables, mushrooms, lentils"
+      }, {
+        flavorId: 2,
+        profile: "Light and Bright",
+        description: "fresh peas, celery, fresh greens, green beans"
+      }, {
+        flavorId: 3,
+        profile: "Spicy",
+        description: "chili pepper and heat"
+      }, {
+        flavorId: 4,
+        profile: "Tangy",
+        description: "citrus and vinegar"
+      }
+    ]
+  }, {
+    id: 2,
+    ingredient: "Fish and Shellfish",
+    flavors: [
+      {
+        flavorId: 1,
+        profile: "Rich",
+        description: "butter and cream"
+      }, {
+        flavorId: 2,
+        profile: "Fried",
+        description: "crispy and oily"
+      }, {
+        flavorId: 3,
+        profile: "Light and Bright",
+        description: "fresh herbs and citrus"
+      }
+    ]
+  }, {
+    id: 3,
+    ingredient: "Poultry",
+    flavors: [
+      {
+        flavorId: 1,
+        profile: "BBQ",
+        description: "sweet, spicy, and tangy"
+      }, {
+        flavorId: 2,
+        profile: "Earthy",
+        description: "root vegetables, mushrooms, lentils"
+      }, {
+        flavorId: 3,
+        profile: "Cheesy or Creamy",
+        description: "cream-based sauces"
+      }
+    ]
+  }, {
+    id: 4,
+    ingredient: "Pork",
+    flavors: [
+      {
+        flavorId: 1,
+        profile: "BBQ",
+        description: "sweet, spicy, and tangy"
+      }, {
+        flavorId: 2,
+        profile: "Light and Earthy",
+        description: "herbs and apples"
+      }, {
+        flavorId: 3,
+        profile: "Asian spiced",
+        description: "salty soy, spicy chili, and citrus sweet"
+      }
+    ]
+  }, {
+    id: 5,
+    ingredient: "Beef",
+    flavors: [
+      {
+        flavorId: 1,
+        profile: "Complex",
+        description: "cumin, harissa, chili, blue cheese..."
+      }, {
+        flavorId: 2,
+        profile: "BBQ",
+        description: "sweet, spicy, and tangy"
+      }, {
+        flavorId: 3,
+        profile: "Steak or Burger",
+        description: "classic and simple"
+      }
+    ]
+  }
+];
+
+module.exports = {
+  food
+};
